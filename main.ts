c3dfec5ce5ecf9558ed2ba4d354491d6ba31f5d8
Dundon <jedundon@gmail.com>
2022-02-13 12:56:16 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+namespace gabe {
+    //% block
+    export function passwordchecker(password: string) {
+        actual_password = "6099"
+        return convertToText(password) == actual_password
+    }
+    let actual_password = ""
+}
